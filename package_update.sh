44b836b370a7f2df99e6d64ff329e8e821659d1a
cnysten <cnysten@student.hive.fi>
2022-05-09 18:34:48 +0300

    package update script

---
+++ package_update.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "[`date`] sudo apt update -y" >> /var/log/update_script.log
+echo "`sudo apt update -y`" >> /var/log/update_script.log
+echo "[`date`] sudo apt update -y" >> /var/log/update_script.log
+echo "`sudo apt update -y`" >> /var/log/update_script.log
