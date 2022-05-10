9fd8fb3b7a1c4041e9b7352a06c8009518ec6cb1
cnysten <cnysten@student.hive.fi>
2022-05-10 16:43:36 +0300

    crontab script

--- monitor_crontab.sh
+++ monitor_crontab.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
+CRONTAB=/etc/crontab
+BACKUP=/etc/crontab.backup
+MESSAGE="There has been a change to the crontab."
 
+if test -f "$BACKUP"; then
+	exit 0
+fi
+
+DIFF=$(diff $CRONTAB $BACKUP)
+
+if [ "$DIFF" != "" ]; then
+	echo MESSAGE | mail -s 'Crontab change' root@debian.lan
+fi
