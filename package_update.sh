02e98029a4672042485c4365fc5fbf44fdca040d
cnysten <cnysten@student.hive.fi>
2022-05-24 13:09:47 +0300

    update script fix

--- package_update.sh
+++ package_update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "[`date`] sudo apt update -y" >> /var/log/update_script.log
 echo "`sudo apt update -y`" >> /var/log/update_script.log
-echo "[`date`] sudo apt update -y" >> /var/log/update_script.log
-echo "`sudo apt update -y`" >> /var/log/update_script.log
+echo "[`date`] sudo apt upgrade -y" >> /var/log/update_script.log
+echo "`sudo apt upgrade -y`" >> /var/log/update_script.log
